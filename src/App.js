973231d4bdee398f98cf2757ade4717d3cb50f53
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-10-15 08:58:19 -0300

    feat (Login): redirects to dashboard when login with success

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import Login from './components/Login'
+import { io } from 'socket.io-client'
 
 const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  );
-};
+  const [socket, setSocket] = useState(null);
+  const [username, setUsername] = useState('')
+  const [room, setRoom] = useState('')
 
-export default App;
+  const handleLogin = (username, room) => {
+    setUsername(username)
+    setRoom(room)
+  }
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:3001/')
+    setSocket(newSocket)
+  }, [setSocket])
+
+  if (!username && !room) {
+    return (
+      <Login 
+        socket={socket} 
+        handleLogin={handleLogin}
+      />
+    )
+  } 
+  return (
+    <div>
+      <h1>{username}</h1>
+      <h2>{room}</h2>
+    </div>
+  );
+}
+
+export default App
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React from 'react'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
-const Login = () => {
+const Login = ({ socket, handleLogin }) => {
+  const handleSubmit = (values) => {
+    socket.emit('login', values, handleLogin(values.username, values.room))
+  }
+
   return (
-    <form aria-label="form">
+    <Formik
+      initialValues={{ username: '', room: '' }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required')
+      })}
+      onSubmit={values => handleSubmit(values)}
+    >
+    <Form>
       <label htmlFor="username">Username</label>
-      <input
-        id="username"
-        name="username"
-        type="text"
-      />
+      <Field name="username" type="text" />
+      <ErrorMessage name="username" />
 
-      <label htmlFor="roomCode">Room Code</label>
-      <input
-        id="roomCode"
-        name="roomCode"
-        type="text"
-      />
-
+      <label htmlFor="Room Code">Room Code</label>
+      <Field name="room" type="text" />
+      <ErrorMessage name="room" />
+      
       <button type="submit">Enter</button>
-    </form>
-  );
-};
+    </Form>
+    </Formik>
+  )
+}
 
-export default Login;
+export default Login

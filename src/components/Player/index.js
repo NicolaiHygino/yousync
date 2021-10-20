14459724fc9e85f3d293bcce3c024a8f66f5fd8a
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-10-20 14:23:03 -0300

    fix (Player): fixed lost refecence of emitState function

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -7,7 +7,7 @@ const PlayerWrapper = styled.div`
   width: 100%;
 `;
 
-let emitStateChange = (e, socket) => {  
+const emitStateChange = (e, socket) => {  
   const position = e.target.getCurrentTime();
   const isPlaying = e.target.getPlayerState();
 
@@ -16,21 +16,22 @@ let emitStateChange = (e, socket) => {
   socket.emit('stateChange', {position, isPlaying});
 };
 
+let handleStateChange = emitStateChange;
+
 const Player = ({ socket }) => {
   const playerRef = useRef(null);
 
   useEffect(() => {
     const player = playerRef.current.internalPlayer;
     socket.on('stateChange', ({position, isPlaying}) => {
-      const oldEmitStateChange = emitStateChange;
-      emitStateChange = () => {};
+      handleStateChange = () => {};
       
       isPlaying === 1 ? player.playVideo() : player.pauseVideo();
       player.seekTo(position);
 
       setTimeout(() => {
-        emitStateChange = oldEmitStateChange;
-      }, 500)
+        handleStateChange = emitStateChange;
+      }, 500);
     })
   }, [socket]);
 
@@ -40,10 +41,9 @@ const Player = ({ socket }) => {
         ref={playerRef}
         videoId="UJBknAsxfrA"
         opts={{height: 'auto', width: '100%'}}
-        onStateChange={e => emitStateChange(e, socket)}
+        onStateChange={e => handleStateChange(e, socket)}
       />
     </PlayerWrapper>
-    
   );
 };
 
